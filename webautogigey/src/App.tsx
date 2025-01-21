e029246abfd18e7b7b65a1139d2e11a6d4b23ac5
Gnanesh-16 <gnaneshbalusa016g@gmail.com>
2025-01-22 00:29:08 +0530

    Add s s buttons

---
+++ webautogigey/src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="app-container">
+      <div className="button-wrapper">
+        <button className="action-btn start">
+          Start Actions
+        </button>
+        <button className="action-btn stop">
+          Stop Actions
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
